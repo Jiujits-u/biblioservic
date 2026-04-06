8f31590c47473fa73274b4a97f157302ebb6b068
Jiujits-u <Glebvalcev182@gmail.com>
2026-04-06 21:38:11 +0300

    add: create dictionary entry model

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
+import org.example.model.DictionaryEntry;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Dictionary application started");
+        DictionaryEntry entry = new DictionaryEntry("abcd", "hello");
+        System.out.println(entry);
     }
 }
